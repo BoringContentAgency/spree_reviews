e74e4a50ffb3d78d80e328d0f8ad9457a223e06d
ishanbluebash <ishan.garg@bluebash.co>
2024-10-21 12:51:05 +0530

    add image functionality

--- app/models/spree/review.rb
+++ app/models/spree/review.rb
@@ -8,6 +8,8 @@ class Spree::Review < ActiveRecord::Base
   after_save :recalculate_product_rating, if: :approved?
   after_destroy :recalculate_product_rating
 
+  has_one_attached :image
+
   validates :name, :review, presence: true
   validates :rating, numericality: {
     only_integer: true,
@@ -21,7 +23,7 @@ class Spree::Review < ActiveRecord::Base
   scope :localized, ->(lc) { where('spree_reviews.locale = ?', lc) }
   scope :most_recent_first, -> { order('spree_reviews.created_at DESC') }
   scope :oldest_first, -> { reorder('spree_reviews.created_at ASC') }
-  scope :preview, ->(review_setting) { limit(review_setting&.preview_size).oldest_first }
+  scope :preview, ->(review_setting) { limit(review_setting&.preview_size).most_recent_first }
   scope :approved, -> { where(approved: true) }
   scope :not_approved, -> { where(approved: false) }
   scope :default_approval_filter, ->(review_setting) { review_setting&.include_unapproved_reviews ? all : approved }
